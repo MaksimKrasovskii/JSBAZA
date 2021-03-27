9bbd0ecde84b9d165b84cf862744ef351c7c41fd
MaksimKrasovskii <maksim.krasovskii@yandex.ru>
2021-03-27 20:53:24 +0300

    создан файл js

---
+++ script.js
@@ -0,0 +1,36 @@
+//К ДЗ работает//
+
+function func1() {
+    var a = 1
+    var b = 2
+
+    var sum = a + b * 10;
+    var dif = (a - b) / 10;
+    var div = a / b;
+    var mul = a * b;
+
+
+
+    alert(dif + mul);
+}
+
+func1();
+
+function func2() {
+    var celsius = 10
+    var fahrenheit = 60
+
+    var temperature = (9 / 5) * celsius + 32;
+
+
+    alert(temperature) + text('по цельсию'); //неверно//
+}
+func2();
+
+function func3() {
+    var admin = name;
+    var name = Василий;
+
+    alert(admin);
+}
+func3();
